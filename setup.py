71f5ce5f8b2df3189219fec6a88156e4d2e7d6b2
inb <inorton@gmail.com>
2019-04-25 07:32:50 +0100

    Allow pushing to pip with non-release version number

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+import os
 from distutils.core import setup
+
+VERSION = "0.0.14"
+SUFFIX = os.getenv("GLE_VERSION_SUFFIX", "")
 
 setup(
     name="gitlab-emulator",
-    version="0.0.14",
+    version=VERSION + SUFFIX,
     description="Run a subset of .gitlab-ci.yml jobs locally",
     author="Ian Norton",
     author_email="inorton@gmail.com",
