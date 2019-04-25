152cb265361c159409df24cbcb9525d27dc9b812
Ian Norton <ian.norton@ncipher.com>
2019-04-25 09:37:54 +0100

    bump to 0.0.15

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-import os
 from distutils.core import setup
 
-VERSION = "0.0.14"
-SUFFIX = os.getenv("GLE_VERSION_SUFFIX", "")
+VERSION = "0.0.15"
 
 setup(
     name="gitlab-emulator",
-    version=VERSION + SUFFIX,
+    version=VERSION,
     description="Run a subset of .gitlab-ci.yml jobs locally",
     author="Ian Norton",
     author_email="inorton@gmail.com",
