ce34a8be054c643f84bc01270975cf9436cb3b7c
inb <inorton@gmail.com>
2021-01-25 10:52:24 +0000

    angular mock working

--- designer-angular/src/app/pipeline/pipeline.component.ts
+++ designer-angular/src/app/pipeline/pipeline.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {interval, Observable} from 'rxjs';
+import {Component, OnInit, ViewChild, AfterViewChecked, ElementRef, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+export interface PipelineJob {
+  name: string;
+}
+
+export interface PipelineDocument {
+  jobs: PipelineJob[];
+}
 
 @Component({
   selector: 'app-pipeline',
   templateUrl: './pipeline.component.html',
   styleUrls: ['./pipeline.component.css']
 })
+@Injectable()
 export class PipelineComponent implements OnInit {
 
-  constructor() { }
+  private timer: Observable<number>;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  pollPipeline(): void {
+    const resp = this.http.get('/api/pipeline', {observe: 'body', responseType: 'json'});
+    resp.subscribe(
+
+    );
+  }
 
   ngOnInit(): void {
+    this.timer = interval(2000);
+    this.timer.subscribe(n => {
+      this.pollPipeline();
+    });
   }
 
 }
