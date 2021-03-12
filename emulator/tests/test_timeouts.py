62398b5a75725b48de3e74d60be37d3bd4ab6d6f
Ian Norton <ian.norton@ncipher.com>
2021-03-12 07:38:22 +0000

    fixes #23 where timeouts did not work for docker jobs

--- emulator/tests/test_timeouts.py
+++ emulator/tests/test_timeouts.py
@@ -1,6 +1,8 @@
 """
 Test that the timeout keyword works
 """
+import time
+
 import pytest
 from gitlabemu import runner, helpers
 
@@ -10,13 +12,16 @@ def test_timeout_set_but_ample(caplog, posix_only, in_tests):
     assert "job run-ok timeout set to 14 mins" in caplog.messages
 
 
-def test_timeout_set_job_slow(caplog, posix_only, in_tests):
+def test_timeout_set_job_slow(caplog, linux_docker, in_tests):
+    start = time.time()
     with pytest.raises(SystemExit):
         runner.run(["--config", "test-timeout.yaml", "run-slow"])
-
+    ended = time.time()
+    assert (ended - start) < 240, "job should not have run for it's max time"
     assert "job run-slow timeout set to 1 mins" in caplog.messages
-    assert "Job exceeded timeout after 60 sec" in caplog.messages
-    assert "Shell job run-slow failed" in caplog.messages
+    assert "Job exceeded 60 sec timeout" in caplog.messages
+    assert "kill container run-slow" in caplog.messages
+    assert "Docker job run-slow failed" in caplog.messages
 
 
 def test_timeout_parser():
