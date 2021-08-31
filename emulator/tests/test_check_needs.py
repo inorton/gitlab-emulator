12f02fbb57fd56a4d16d76ebeace357df806e470
Ian Norton <ian.norton@ncipher.com>
2021-08-31 17:36:19 +0100

    re #46 TDD add test for stageless needs

--- emulator/tests/test_check_needs.py
+++ emulator/tests/test_check_needs.py
@@ -4,11 +4,18 @@ import os
 from gitlabemu import runner
 
 
-def test_run_needs(in_tests):
-    runner.run(["--config", "test-needs.yaml", "--full", "finish"])
+def test_run_needs_stages(in_tests, envs):
+    os.environ["GLE_CONFIG"] = os.path.join(os.getcwd(), "gle-config-v14.1.yaml")
+    runner.run(["--config", "test-needs-14.1.yaml", "--full", "finish"])
 
 
-def test_illegal_needs_early(in_tests, capsys):
+def test_run_needs_no_stages(in_tests, envs):
+    os.environ["GLE_CONFIG"] = os.path.join(os.getcwd(), "gle-config-v14.2.yaml")
+    runner.run(["--config", "test-needs-14.2.yaml", "--full", "finish"])
+
+
+def test_illegal_needs_early(in_tests, capsys, envs):
+    os.environ["GLE_CONFIG"] = os.path.join(os.getcwd(), "gle-config-v14.1.yaml")
     with pytest.raises(SystemExit):
         runner.run([
             "--list",
@@ -19,7 +26,8 @@ def test_illegal_needs_early(in_tests, capsys):
     assert "job job1 needs job2 that is not in an earlier stage" in stderr
 
 
-def test_illegal_needs_same(in_tests, capsys):
+def test_illegal_needs_same(in_tests, capsys, envs):
+    os.environ["GLE_CONFIG"] = os.path.join(os.getcwd(), "gle-config-v14.1.yaml")
     with pytest.raises(SystemExit):
         runner.run([
             "--list",
