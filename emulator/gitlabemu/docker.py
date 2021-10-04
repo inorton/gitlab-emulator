8a61bfa46a6d5a9f34a7d6748832280ba29cadf5
Ian Norton <ian.norton@ncipher.com>
2021-10-04 12:16:59 +0100

    fix CI_JOB_IMAGE when an image is configured as a map

--- emulator/gitlabemu/docker.py
+++ emulator/gitlabemu/docker.py
@@ -186,7 +186,10 @@ class DockerJob(Job):
         super(DockerJob, self).load(name, config)
         all_images = config.get("image", None)
         self.image = config[name].get("image", all_images)
-        self.configure_job_variable("CI_JOB_IMAGE", self.image)
+        image_source = self.image
+        if isinstance(self.image, dict):
+            image_source = self.image.get("name")
+        self.configure_job_variable("CI_JOB_IMAGE", image_source)
         self.services = get_services(config, name)
 
     def abort(self):
