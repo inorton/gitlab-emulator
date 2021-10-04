cf2b54b28792a39c510b2fe404ec51e1bc341438
Ian Norton <ian.norton@ncipher.com>
2021-10-04 21:30:32 +0100

    bump versions

--- emulator/setup.py
+++ emulator/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-VERSION = "0.8.3"
+VERSION = "0.8.4"
 
 setup(
     name="gitlab-emulator",
--- runner/GitlabPyRunner/consts.py
+++ runner/GitlabPyRunner/consts.py
@@ -2,5 +2,5 @@
 Constants for gitlab-python-runner
 """
 NAME = "gitlab-python-runner"
-VERSION = "14.3.2"
+VERSION = "14.3.3"
 USER_AGENT = "{} {}".format(NAME, VERSION)
