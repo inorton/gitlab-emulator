85efe8ef6ced8b30c9b5a1bb79f7316d252f666e
Ian Norton <ian.norton@ncipher.com>
2022-05-13 20:02:50 +0100

    aaaaa

--- emulator/gitlabemu/docker.py
+++ emulator/gitlabemu/docker.py
@@ -240,7 +240,7 @@ class DockerJob(Job):
             while attempts > 0:
                 try:
                     cmdline = self.shell_command(target_script)
-                    task = self.docker.exec(self.workspace, cmdline, user=user, pipe=stdin is not None)
+                    task = self.docker.exec(self.workspace, cmdline, user=user, pipe=True)
                     self.communicate(task, script=stdin)
                     break
                 except DockerExecError:
